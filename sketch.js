e42f482559c47cf1ceb9d8c4d871aea95e9d1137
Eina Onting <einers@outlook.com>
2020-04-27 21:48:19 -0700

    use nice color palettes for more generative looking art

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(palettes).slice(0, colorCount));
+  console.log(palette);
+
   // returns points on a grid
   const createGrid = () => {
     const points = [];
-    const count = 35;
+    const count = 40;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
@@ -19,7 +24,11 @@ const sketch = () => {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
 
-        points.push([u, v]);
+        points.push({
+          color: random.pick(palette),
+          radius: Math.abs(0.0035 + random.gaussian() * 0.01),
+          position: [u, v],
+        });
       }
     }
 
@@ -36,16 +45,19 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const { color, position, radius } = data;
+      const [u, v] = position;
       // scaling points up back to pixel size
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.strokeStyle = "black";
-      context.lineWidth = 20;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.strokeStyle = "black";
+      // context.lineWidth = 20;
+      context.fillStyle = color;
+      context.fill();
     });
   };
 };
