6618570d18a712c813e238419799f21cf5c4511c
Eina Onting <einers@outlook.com>
2020-04-27 21:06:24 -0700

    explore settings features

--- sketch.js
+++ sketch.js
@@ -2,24 +2,26 @@ const canvasSketch = require("canvas-sketch");
 
 const settings = {
   dimensions: "A4",
+  units: "cm",
+  pixelsPerInch: 300,
   // dimensions: [2048, 2048],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "steelblue";
-    context.fillRect(5, 0, width, height);
+    context.fillRect(0, 0, width, height);
 
     // circle
     // start with a path
     context.beginPath();
     // Math.PI * 2 = the end point, Math.PI is a semicircle
-    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
+    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
 
     context.fillStyle = "lightsteelblue";
     context.fill();
 
-    context.lineWidth = 20;
+    context.lineWidth = width * 0.05;
     context.strokeStyle = "blue";
     context.stroke();
   };
