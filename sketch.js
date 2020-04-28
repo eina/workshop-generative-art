b1d3a6c438244a9de240f95c569a9e75107459c4
Eina Onting <einers@outlook.com>
2020-04-27 21:23:15 -0700

    create grid

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: "A4",
-  units: "cm",
-  pixelsPerInch: 300,
-  // dimensions: [2048, 2048],
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  // returns points on a grid
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        // between 0 and 1
+        // 0.5, 0.5 should be the center
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+
+        points.push([u, v]);
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = "steelblue";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    // circle
-    // start with a path
-    context.beginPath();
-    // Math.PI * 2 = the end point, Math.PI is a semicircle
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
+    points.forEach(([u, v]) => {
+      // scaling points up back to pixel size
+      const x = u * width;
+      const y = v * height;
 
-    context.fillStyle = "lightsteelblue";
-    context.fill();
-
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = "blue";
-    context.stroke();
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 40;
+      context.stroke();
+    });
   };
 };
 
