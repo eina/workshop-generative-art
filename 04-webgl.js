b06fbd6a30b068387ecab3cb63f124a9ed024085
Eina Onting <einers@outlook.com>
2020-04-28 18:51:53 -0700

    animate three.js

--- 04-webgl.js
+++ 04-webgl.js
@@ -5,8 +5,15 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
+const eases = require("eases");
+const BezierEasing = require("bezier-easing");
 
 const settings = {
+  dimensions: [512, 512],
+  fps: 24,
+  duration: 4,
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
@@ -25,27 +32,36 @@ const sketch = ({ context }) => {
   renderer.setClearColor("#fff", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(6, 2, 5);
-  camera.lookAt(new THREE.Vector3());
-
-  // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
+  const camera = new THREE.OrthographicCamera();
 
   // Setup your scene
   const scene = new THREE.Scene();
 
+  const palette = random.pick(palettes);
+  const box = new THREE.BoxGeometry(1, 1, 1);
   // workshop code
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({
-      color: "red",
-      roughness: 0.75,
-      flatShading: true,
-    })
-  );
+  for (let i = 0; i < 40; i++) {
+    const mesh = new THREE.Mesh(
+      box,
+      new THREE.MeshStandardMaterial({
+        color: random.pick(palette),
+        roughness: 0.75,
+        flatShading: true,
+      })
+    );
+    // x, y, z
+    mesh.position.set(random.range(-1, 1), random.range(-1, 1), random.range(-1, 1));
+    mesh.scale.set(random.range(-1, 1), random.range(-1, 1), random.range(-1, 1));
+    mesh.scale.multiplyScalar(0.45);
+    scene.add(mesh);
+  }
 
-  scene.add(mesh);
+  // add lighting
+  scene.add(new THREE.AmbientLight("hsl(0%, 0%, 20%)"));
+  const light = new THREE.DirectionalLight("white", 1);
+  // the sun!
+  light.position.set(0, 0, 4);
+  scene.add(light);
 
   // Setup a geometry
   // const geometry = new THREE.SphereGeometry(1, 32, 16);
@@ -60,23 +76,44 @@ const sketch = ({ context }) => {
   // const mesh = new THREE.Mesh(geometry, material);
   // scene.add(mesh);
 
+  const easeFn = BezierEasing(0.31, 0.89, 0.61, 0.42);
+
   // draw each frame
   return {
     // Handle resize events here
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight, false);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 1.75;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render({ time }) {
-      controls.update();
+    render({ playhead }) {
+      const t = Math.sin(playhead * Math.PI);
+      scene.rotation.z = easeFn(t);
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
       renderer.dispose();
     },
   };
