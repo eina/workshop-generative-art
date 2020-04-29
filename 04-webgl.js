51786d987905928d7cb9ef6ad6b70cf607fb224a
Eina Onting <einers@outlook.com>
2020-04-28 17:09:53 -0700

    add webgl.js for three.js

---
+++ 04-webgl.js
@@ -0,0 +1,85 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl",
+  // turn on MSAA
+  attributes: { antialias: true },
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas,
+  });
+
+  // WebGL background color
+  renderer.setClearColor("#fff", 1);
+
+  // Setup a camera
+  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
+  camera.position.set(6, 2, 5);
+  camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // workshop code
+  const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({
+      color: "red",
+      roughness: 0.75,
+      flatShading: true,
+    })
+  );
+
+  scene.add(mesh);
+
+  // Setup a geometry
+  // const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+  // // Setup a material
+  // const material = new THREE.MeshBasicMaterial({
+  //   color: "red",
+  //   wireframe: true,
+  // });
+
+  // // Setup a mesh with geometry + material
+  // const mesh = new THREE.Mesh(geometry, material);
+  // scene.add(mesh);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      camera.aspect = viewportWidth / viewportHeight;
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      controls.update();
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      controls.dispose();
+      renderer.dispose();
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
